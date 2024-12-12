9971c81efbdff084c675daf54d80752ffc813146
広登 小野寺 <hirorig10@outlook.jp>
2024-12-12 22:08:53 +0900

    secondCommit

---
+++ src/routes/AssemblyRoutes.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+import { Routes, Route, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
+
+import AnimatedRouteSet from "./AnimatedRouteSet"
+
+import HomePage from "../pages/HomePage"
+import Test from "../test/Test"
+
+
+const routeList = [
+    { path: "/", element: <HomePage /> },
+    { path: "/test", element: <Test /> }
+]
+
+const AssemblyRoutes = () => {
+    const location = useLocation(); // location を取得
+
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                {routeList.map(({ path, element }) => (
+                    <Route
+                        key={path} // key を指定
+                        path={path}
+                        element={<AnimatedRouteSet>{element}</AnimatedRouteSet>}
+                    />
+                ))}
+            </Routes>
+        </AnimatePresence>
+    );
+
+}
+
+
+export default AssemblyRoutes;
